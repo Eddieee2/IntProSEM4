1d86e206ef83de089d80ef91b61ad43c5371a9e0
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 10:37:11 -0600

    ejercicio7.py

---
+++ Ejercicio7.py
@@ -0,0 +1,13 @@
+#Valores iniciales
+a = 5
+b = 10
+
+print ("Antes del intercambio: ")
+print ("a = ", a, "b = ", b)
+
+aux = a 
+a = b 
+b = aux
+
+print ("\nDespues del intercambio: ")
+print ("a = ", a, "b = ", b)
