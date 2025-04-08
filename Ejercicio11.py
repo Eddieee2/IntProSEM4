3002f603546b746b2287afc416406837f1b16aaa
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 13:25:04 -0600

    ejercicio111.py

---
+++ Ejercicio11.py
@@ -0,0 +1,11 @@
+edadUsuario = 25
+precioTotalCarrito = 150.75
+estadoInicioSesion = True 
+nombreCursoActual = "Introduccion a la Programacion"
+temperaturaPromedio = 22.5 
+
+print("Edad del usuario:", edadUsuario)
+print("Precio total del carrito de compras:", precioTotalCarrito)
+print("Estado de inicio de sesión:", estadoInicioSesion)
+print("Nombre del curso actual:", nombreCursoActual)
+print("Temperatura promedio registrada:", temperaturaPromedio)
