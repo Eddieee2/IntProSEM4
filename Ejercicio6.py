e28c25f70f7ee37f3292ace84879b60771e32c16
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 10:32:08 -0600

    ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,11 @@
+# Datos de entrada
+año_nacimiento = 2005
+año_actual = 2025
+
+# Cálculos
+edad_actual = año_actual - año_nacimiento
+edad_en_10_años = edad_actual + 10
+
+# Mostrar resultados
+print("Edad actual:", edad_actual)
+print("Edad en 10 años:", edad_en_10_años)
