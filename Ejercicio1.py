6d997e6b1ab3dfac003b26100986caf633b93b10
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 09:29:38 -0600

    IntProSEM4

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+#declarar variables
+
+nombre_completo = "Jose Cristo Carvalo Herrera"
+edad = 17
+altura = 1.72
+soltero = True
+
+print (f"Nombre completo: {nombre_completo}")
+print (f"Tipo de dato: {type(nombre_completo)}")
+print (f"Edad: {edad}")
+print (f"Tipo de dato: {type(edad)}")
+print (f"Altura: {altura}")
+print (f"Tipo de dato: {type(altura)}")
---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+#tipo de datos numericos 
+
+cantidad = 10
+precio = 130
+total = cantidad * precio
+
+print (f"Cantidad: {cantidad:>3}")
+print (f"Precio: {precio:>5}")
+print (f"Total: {total:>6}")
