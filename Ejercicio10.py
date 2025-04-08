69f9d09c6201cb450c1964998c37d9cc4d1da455
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 11:22:32 -0600

    ejercicio10.py

---
+++ Ejercicio10.py
@@ -0,0 +1,21 @@
+# 1. Nombre completo del estudiante (camelCase)
+nombreCompletoEstudiante = "Eduardo Coe"
+
+# 2. Número de matrícula (snake_case)
+numero_matricula = 12345
+
+# 3. Total de ventas del mes (PascalCase)
+TotalVentasDelMes = 5000
+
+# 4. Límite máximo de intentos (SCREAMING_SNAKE_CASE)
+MAXIMO_INTENTOS = 3
+
+# 5. Cantidad de productos (camelCase)
+cantidadProductos = 100
+
+
+print("Nombre completo del estudiante:", nombreCompletoEstudiante)
+print("Número de matrícula:", numero_matricula)
+print("Total de ventas del mes:", TotalVentasDelMes)
+print("Límite máximo de intentos:", MAXIMO_INTENTOS)
+print("Cantidad de productos:", cantidadProductos)
