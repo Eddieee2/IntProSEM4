c09ae7c2ed59ad0f5d4e1b26fc3e0eab93c3ee27
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 14:43:31 -0600

    ejercicio20.py

---
+++ Ejercicio20.py
@@ -0,0 +1,9 @@
+edad_actual = int(input("Ingrese su edad actual: "))
+
+edad_5_anos = edad_actual + 5
+edad_10_anos = edad_actual + 10
+edad_20_anos = edad_actual + 20
+
+print(f"En 5 años, tendras {edad_5_anos} años.")
+print(f"En 10 años, tendras {edad_10_anos} años.")
+print(f"En 20 años, tendras {edad_20_anos} años.")
