f4601b796ed2aabe8786ad8813911d89bcae0b95
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 11:13:03 -0600

    ejercicio9.py

---
+++ Ejercicio9.py
@@ -0,0 +1,16 @@
+# Correcciones de nombres de variables
+
+# camelCase
+nombrePersona = "Carlos"
+totalPrecio = 150
+direccionEmail = "ejemplo@correo.com"
+
+# snake_case
+edad_persona = 30
+numero_de_telefono = "123-456-7890"
+
+print("nombrePersona:", nombrePersona)
+print("totalPrecio:", totalPrecio)
+print("direccionEmail:", direccionEmail)
+print("edad_persona:", edad_persona)
+print("numero_de_telefono:", numero_de_telefono)
