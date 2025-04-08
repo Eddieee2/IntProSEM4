fa11f1b9a49ecfaf2c00da5b3aa69ba41c27468e
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 14:41:11 -0600

    ejercicio18.py

--- Ejercicio18.py
+++ Ejercicio18.py
@@ -1,2 +1,6 @@
 tareas = float(input("Ingrese la calificacion de las tareas (0-100):"))
 examen_parcial = float(input("Ingrese la calificacion del examen parcial (0-100):"))
+examen_final = float(input("Ingrese la califacion del examen final (0-100): "))
+
+calificacion_final = (tareas * 0.30) + (examen_parcial * 0.30) + (examen_final * 0.40)
+print(f"La calificacion final es:  {calificacion_final:.2f}")
---
+++ Ejercicio19.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+
+correo = f"{nombre.lower()}.{apellido.lower()}@miuniversidad.edu.ni"
+
+print(f"El correo electrónico es: {correo}")
