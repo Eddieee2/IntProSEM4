626195081d9f6495bbf8f6e9e5dd378c314b5091
Eddieee2 <lescoe1234@gmail.com>
2025-04-08 14:56:09 -0600

    ejercicio23.py

---
+++ Ejercicio23.py
@@ -0,0 +1,4 @@
+precio_compra = float(input("Ingresa el precio de compra del articulo: "))
+precio_venta = precio_compra * 1.30
+
+print(f"El precio de venta para obtener una ganancia del 30% es {precio_venta:.2f}")
